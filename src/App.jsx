3e22006ae2f3cde14285c3822ce09d0171f3648b
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-09-20 05:30:43 +0000

    Added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./Pages/About";
 import HomePage from "./Pages/HomePage";
+import Notfound from "./Pages/Notfound";
 function App() {
   return (
     //using layout
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/about" element={<About/>}/>
-
+        <Route path = "*" element= {<Notfound/>}/>
 
       </Routes>
     </>
---
+++ src/Pages/Notfound.jsx
@@ -0,0 +1,22 @@
+import {useNavigate } from "react-router-dom";
+
+export default function Notfound(){
+    return(
+        <div className = "h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+            <h1 className = "text-9xl font-extrabold text-white tracking-wider">
+                404
+            </h1>
+            <div className="bg-black text-white px-2 text-sm rounded rotate-12 absolute">PAGE NOT FOUND ....</div>
+            <button className="mt-5">
+                <a className="relative inline-block text-sm font-medium text-[#ff6A3D group active:text-yellow-500 focus:outline-none focus:out ">
+                    <span  onClick = {() => useNavigate(-1) }className = "relative block px-8 bg-[#1a2238] border border-current">
+                    GO Back
+                     </span>
+                </a>
+
+
+            </button>
+
+        </div>
+    )
+}
