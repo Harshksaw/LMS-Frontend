84cc51b9160740c119d67ec87f08338ba18ff796
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-10-22 12:31:50 +0530

    Registration Done||AuthSlice

--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -1,15 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
+
+import axiosInstance from "../../Config/axiosinstance";
 
 const initialState = {
     isLoggedIn: localStorage.getItem("isLoggedIn") || false,
     role: localStorage.getItem("role") || "",
-    data: localStorage.getItem("data") || {}
-}
+    data: localStorage.getItem("data") || {},
+};
+
+//thunk -> a logic that will perform work later
+
+// function to handle signup
+export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
+    try {
+        let res = axiosInstance.post("user/register", data);
+        // function to display notifications during the asynchronous process:
+        toast.promise(res, {
+            loading: "Wait! Creating your account",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to create account",
+        });
+
+        // getting response resolved here
+        res = await res;
+        return res.data;
+    } catch (error) {
+        toast.error(error?.response?.data?.message);
+    }
+});
 
 const authSlice = createSlice({
     name: "auth",
     initialState,
-    reducers: {}
+    reducers: {},
 });
 
 export default authSlice.reducer;
