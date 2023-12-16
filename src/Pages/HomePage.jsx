3b49915e4f91adcbf3e545e48ddec6dc946aa6d5
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-12-16 09:51:17 +0000

    changes pictures

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import  img3 from "../Assets/Images/img3.jpeg";
+import img2 from "../Assets/Images/homePageMainImage.png";
+import img3 from "../Assets/Images/img3.jpeg";
 import HomeLayout from "../Layouts/HomeLayout";
 
 export default function HomePage() {
+  const pictureUrls = [
+    img3,
+    img2,
+    // Add more picture URLs as needed
+  ];
+  const [currentPictureIndex, setCurrentPictureIndex] = useState(0);
+
+  useEffect(() => {
+    // Set up automatic switching every 5 seconds (adjust the interval as needed)
+    const intervalId = setInterval(() => {
+      setCurrentPictureIndex((prevIndex) =>
+        prevIndex === pictureUrls.length - 1 ? 0 : prevIndex + 1,
+      );
+    }, 2000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
   return (
     <HomeLayout>
       <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[90vh]">
@@ -18,12 +39,12 @@ export default function HomePage() {
           </p>
         </div>
         <div className="space-x-6">
-          <Link to= '/courses' >
+          <Link to="/courses">
             <button className="bg-yellow-400 my-2 px-7 py-3 rounded-md font-semibold text-lg cursor-pointer  hover:bg-yellow-600 transition-all ease-in-out duration-300">
               Explore Courses
             </button>
           </Link>
-          <Link to= '/contact' >
+          <Link to="/contact">
             <button className="  border bg-yellow-400 my-2 px-5 py-3  rounded-md font-semibold text-lg cursor-pointer  hover:bg-yellow-800 transition-all ease-in-out duration-3">
               Contact Us
             </button>
@@ -31,8 +52,13 @@ export default function HomePage() {
         </div>
 
         <div className="w-1/2 flex items-center justify-center">
-            <img alt="" src={img3}/>
-
+          <div>
+            <img
+              src={pictureUrls[currentPictureIndex]}
+              alt={`Picture ${currentPictureIndex + 1}`}
+              style={{ maxWidth: "100%" }}
+            />
+          </div>
         </div>
       </div>
     </HomeLayout>
