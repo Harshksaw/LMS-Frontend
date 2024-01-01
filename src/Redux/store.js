90b9215d033f4d20bbff79cca3d42ae9ec37f135
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-01-01 18:08:34 +0530

    Complete|| DEPLOY

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,17 +3,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from './Slices/AuthSlice';
 import courseSliceReducer from './Slices/CourseSlice';
 import lectureSliceReducer from './Slices/LectureSlice';
-import razorpaySliceReducer from './Slices/RazorpaySlice';
-import statSliceReducer from './Slices/StatSlice';
+
 
 
 const store = configureStore({
     reducer: {
         auth: authSliceReducer,
         course: courseSliceReducer,
-        razorpay: razorpaySliceReducer,
+
         lecture: lectureSliceReducer,
-        stat: statSliceReducer
+
     },
     devTools: true
 });
