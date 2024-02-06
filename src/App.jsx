db762f04077d107f34ad80bc9392fc64e02ac23f
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-02-06 21:58:36 +0530

    Add ViewCourse page and VideoDetails component

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ import AddCourse from './components/core/Dashboard/AddCourse'
 import MyCourses from './components/core/Dashboard/MyCourses'
 import EditCourse from './components/core/Dashboard/EditCourse'
 import Catalog from "./pages/Catalog"
+import ViewCourse from './pages/ViewCourse'
 
 function App() {
   const dispatch = useDispatch()
@@ -86,6 +87,26 @@ function App() {
           </>
         )
         }
+
+        <Route element={
+          <PrivateRoute>
+          <ViewCourse/>
+          </PrivateRoute>
+        }>
+        {
+          user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            <>
+         <Route 
+            path="view-course/:courseId/section/:sectionId/sub-section/:subSectionId"
+            element={<VideoDetails />}
+          />
+
+            </>
+          )
+
+        }
+        </Route>
+
 
        
 
